03d4178974b79de50d79cfa9f42cc3e26144bc9d
Andy Bell <me@andy-bell.co.uk>
2015-08-26 14:39:39 +0100

    Add a load of useful helpers

--- front-end/scripts/_helpers.js
+++ front-end/scripts/_helpers.js
@@ -1,21 +1,175 @@
 (function($) {
 	
-/*------------------------------------*\
-    ANY
-    
-    This will return true if there are any items 
-    in a jQuery collection. 
-    
-    EXAMPLE
-    
-    var items = $(".item");
-    
-    if(items.any()) {
-		console.log("YAY!");
-	}
-\*------------------------------------*/
+	/*------------------------------------*\
+	    ANY
+	    
+	    This will return true if there are any items 
+	    in a jQuery collection. 
+	    
+	    EXAMPLE
+	    
+	    var items = $(".item");
+	    
+	    if(items.any()) {
+			console.log("YAY!");
+		}
+	\*------------------------------------*/
 	
 	$.fn.any = function() {
 		return $(this).length > 0;
 	}
+
+
+	/*------------------------------------*\
+	    DATE
+	    
+	    Simple date helpers
+
+	\*------------------------------------*/
+	$.extend({
+		date: {
+			getYear: function() {
+				var date = new Date();
+
+				return date.getFullYear();
+			}
+		}
+	});
+
+    /*------------------------------------*\
+        PARSE SETTINGS
+        
+        This will try and parse inline json settings as an object literal to pass into a plugin
+        
+        EXAMPLE
+        
+        <div class="item" data-settings='{"setting1": true}'></div>
+
+        var item = $(".item"),
+            settings = item.parseSettings();
+        
+        console.log(settings.setting1);
+        
+        returns true;
+
+    \*------------------------------------*/
+    $.fn.parseSettings = function () {
+
+        var elem = $(this),
+            response = {};
+
+        if (elem.attr("data-settings")) {
+
+            try {
+                response = $.parseJSON(elem.attr("data-settings"));
+            }
+            catch (ex) {
+                $.log("Check input data. Message: " + ex.message);
+                return {};
+            }
+        }
+
+        return response;
+    };
+    
+
+    /*------------------------------------*\
+        AJAX REQUEST
+        
+        A nice Ajax wrapper method
+        
+        EXAMPLE
+        
+        $.ajaxRequest({
+            url: "/test",
+            callback(function(data, isSuccess) {
+                
+                if(isSuccess) {
+                    alert('All the data is WINNING');
+                }
+            });
+        });
+
+    \*------------------------------------*/
+	$.extend({
+		ajaxRequest: function(options) {
+			
+			var settings = {
+                dataType: "application/json",
+                url: "/",
+                data: {},
+                method: "GET",
+                callback: null
+			};
+			
+			var init = function() {
+				
+				settings = $.extend(true, {}, settings, options);
+
+				$.ajax({
+					contentType: settings.dataType, 
+					url: settings.url,
+					data: settings.data,
+					type: settings.method,
+					success: function(responseData) {
+						tryCallback(responseData);
+					},
+					error: function(responseData) {
+						tryCallback(responseData);
+					}
+				});
+			},
+			
+			tryCallback = function(responseData) {
+				
+				if(typeof(settings.callback) == "function") {
+					settings.callback(responseData, (responseData != null ? (responseData.status == 200 ? false : true) : true));
+				}
+			}
+			
+			init();
+			
+		}
+	});
+	
+	
+    /*------------------------------------*\
+        ESC
+        
+        A useful little wrapper for the escape key press event
+        
+        EXAMPLE
+        
+		$.esc({
+			callback: function(evt) {
+				
+				// Close your modal or whatever. Accessibility FTW
+			}
+		});
+
+    \*------------------------------------*/
+	$.extend({
+		esc: function(options) {
+			
+			var settings = {
+				callback: null
+			}
+			
+			settings = $.extend(true, {}, settings, options);
+			
+			if(typeof(settings.callback) == 'function') {
+				
+				$(document).keyup(function(evt) {
+					
+					// escape key maps to keycode `27`
+					if (evt.keyCode == 27) { 
+						
+						// run callback and pass the event over
+						settings.callback(evt);
+					}
+				});	
+			}
+
+		}
+	});
 }($));
